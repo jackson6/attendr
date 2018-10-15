b9e4117a99dd73cfbcce025ed38269b6fb541a7d
odane <odane@ctrl-it.com.jm>
2018-10-14 21:26:59 -0400

    cors

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const logger = require('morgan');
 
 const bodyParser = require('body-parser');
 
+const cors = require('cors')
+
 const app = express();
 
 app.use(logger('dev'));
+
+app.use(cors())
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
